5510756ff6f9802b3130d68728b0298f750c4ff0
FunaKatu <katuto.funaoka2000@gmail.com>
2019-10-09 11:45:21 +0900

    カメラ視点切り替え追加

---
+++ Assets/Scripts/Camera_ChangePoint.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_ChangePoint : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] Transform look_point;
+    [SerializeField] Transform point_near;
+    [SerializeField] Transform point_far;    
+    [SerializeField] float     speed;
+
+    private bool now_near;
+
+
+    void Start()
+    {
+        now_near = true;
+    }
+
+
+    void Update()
+    {
+        target.localPosition *= 1f / (1f + (speed * Time.deltaTime));
+
+        target.LookAt(look_point);
+    }
+
+
+    //---------------
+    // タップしたら
+    //---------------
+    public void Tap()
+    {
+        if (now_near)
+        {
+            target.parent = point_far;
+        }
+        else
+        {
+            target.parent = point_near;
+        }
+
+        now_near = !now_near;
+    }
+}
